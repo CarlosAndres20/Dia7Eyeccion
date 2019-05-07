159635e52e8afe5b02b00ca82ddef9334e45c972
Sebastian <>
2019-05-07 17:53:25 -0500

    dwe

--- Interface1.cs
+++ Interface1.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace Dia7Eyeccion
 {
-    interface Interface1
+    interface IArma
     {
+        void Disparar(string nombre);
     }
 }
--- Soldado.cs
+++ Soldado.cs
@@ -4,26 +4,30 @@ using System.Text;
 
 namespace Dia7Eyeccion
 {
-    class Soldado
+    class Soldado : IArma
     {
-        public string Antiguedad { get; set; }
-        public string Nombre { get; set; }
-        public string NombreBatallon { get; set; }
+        /* public string Antiguedad { get; set; }
+         public string Nombre { get; set; }
+         public string NombreBatallon { get; set; }
 
-        Escopeta esc=new Escopeta();
-        Metralleta met=new Metralleta();
-        Rifle rif=new Rifle();
-        public void DispararEscopeta(string nombre)
+         Escopeta esc=new Escopeta();
+         Metralleta met=new Metralleta();
+         Rifle rif=new Rifle();
+         public void DispararEscopeta(string nombre)
+         {
+             esc.Disparar(nombre);
+         }
+         public void DispararMetralleta(string nombre)
+         {
+             met.Disparar(nombre);
+         }
+         public void DispararRifle(string nombre)
+         {
+             rif.Disparar(nombre);
+         }*/
+        public void Disparar(string nombre)
         {
-            esc.Disparar(nombre);
-        }
-        public void DispararMetralleta(string nombre)
-        {
-            met.Disparar(nombre);
-        }
-        public void DispararRifle(string nombre)
-        {
-            rif.Disparar(nombre);
+            throw new NotImplementedException();
         }
     }
 }
